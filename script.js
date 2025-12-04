6d13b385de2305f6ff771c826a7839d5e6befebc
Benita Almroth <benita.almroth@hotmail.com>
2025-12-04 21:24:07 +0100

    wip: added button eventlist, working on randomizer

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
 // fetch variabler
-const urlCategory = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`; 
 const urlAllCat = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
-const urlIngredient = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast';
+const urlIngredient = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast'; //möjlighet att skriva in ingrediens?
 
 //select sektion anropa html element 
 const cat = document.getElementById('cat'); //för att lägga in options i select tagen
-const button = document.getElementsByClassName('generate'); //för att klicka på knapp -> be js generera
+const button = document.getElementById('generate'); //för att klicka på knapp -> be js generera
 const result = document.getElementById('result'); //lägg in info :) 
 
-// element som behöver genereras via js = h2(recept namn), img(bild), list(ingridienser), p(instruktioner)
-
+// element som behöver genereras via js = h2(recept namn), img(bild), list(ingredienser), p(instruktioner)
 
 //dropdown val
 fetch(urlAllCat)
-    .then((response) => {
+.then((response) => {
     return response.json();  
-    })
-     .then((allCat) => {
-        console.log(allCat)
-        allCat.meals.forEach(meal => {
-            const option = document.createElement("option");
-            option.value = meal.strCategory;
-            option.text = meal.strCategory;
-
-            cat.appendChild(option);
-        });
+})
+.then((allCat) => { //allCat = array nament allCat['meals'] foreach meals as meal
+    allCat.meals.forEach(meal => {
+        const option = document.createElement("option");
+        option.value = meal.strCategory;
+        option.text = meal.strCategory;
+        
+        cat.appendChild(option);
+    });
 });
+
+
+//när man har valt cat ->
+button.addEventListener("click", () => {
+    const category = cat.value;
+
+    const urlCategory = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`;
+
+    fetch(urlCategory)
+    .then((response) => {
+        return response.json();
+    })
+    .then((allMeals) => { // allMeals['meals'] = alla måltider inom kategorin???
+        const meals = allMeals.meals;
+        const randomMeal = //???????
+        console.log(meals);
+
+        const urlDetails = `https://www.themealdb.com/api/json/v1/1/search.php?s=${randomMeal}`;
+
+        
